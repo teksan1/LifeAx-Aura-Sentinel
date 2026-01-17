7d7bdd34aed5d2eb3604b4988b8fd856772b5e52
teksan1 <teksan1@example.com>
2026-01-17 06:15:17 -0500

    Android Build: Initialized Capacitor and Android project structure

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'im.manus.lifeax',
+  appName: 'LifeAx',
+  webDir: 'dist'
+};
+
+export default config;
